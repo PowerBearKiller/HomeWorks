d2540d7b0f45beea3ae924dd1c1b0ff670f85b30
Artur <PowerBearKiller@mail.ru>
2021-12-12 22:40:58 +0300

    Lesson1Commit

---
+++ HomeWork1.2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Баранович Артур
+//а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2, y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2 - y1, 2).Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+//б) *Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.
+
+Console.WriteLine("ВВедите  X числа А");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Y числа А");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("ВВедите X числа B");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Y числа В");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine("Растоение между точками " + string.Format("{0:F2}", r));
+
+//пункт б
+
+ void rast()
+{
+    double R1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    Console.WriteLine("Растоение между точками " + string.Format("{0:F2}", R1));
+}
+
+rast();
---
+++ HomeWork1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Баранович Артур
+//1.Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку:
+//а) используя склеивание;
+//б) используя форматированный вывод;
+//в) используя вывод со знаком $.
+
+Console.WriteLine("Введите ваше имя");
+string name = Console.ReadLine();
+Console.WriteLine("Введите вашу фамилию");
+string secandndname = Console.ReadLine();
+Console.WriteLine("Введите ваш возраст");
+string age = Console.ReadLine();
+Console.WriteLine("Введите ваш рост");
+string hight = Console.ReadLine();
+Console.WriteLine("Введите ваш вес");
+string wight = Console.ReadLine();
+
+Console.WriteLine("Используя склеевание \nВаши данные: Имя - " +name + " ваша фамилия - " + secandndname + " Ваш возраст - " + age + " Ваш рост - " + hight + " Ваш вес - "+ wight);
+Console.WriteLine("Используя форматировнный вовыд \nВаши данные: Имя - {0} ваша фамилия - {1} Ваш возраст - {2} Ваш рост - {3} Ваш вес - {4}", name, secandndname,age,hight,wight );
+Console.WriteLine($"Используя вывод со знаком $ \nВаши данные: Имя - {name} ваша фамилия - {secandndname} Ваш возраст - {age} Ваш рост - {hight} Ваш вес - {wight}");
