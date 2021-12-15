043d26153bd03d6687b6a0b4680d419d6fe5ecfc
Artur <PowerBearKiller@mail.ru>
2021-12-15 22:33:16 +0300

    1stCommitLesson2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Баранович Артур
+//2. Написать метод подсчета количества цифр числа.
+Console.WriteLine("Введите число");
+            int a = Int32.Parse(Console.ReadLine());
+            int s = 0;
+            while(a != 0)
+            {
+                a = a/10;
+                s++;
+                
+            }
+            Console.WriteLine(s);
---
+++ HomeWork5/Program.cs
@@ -0,0 +1,29 @@
+﻿//Баранович Артур
+//5.
+//а) Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или всё в норме.
+//б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+Console.WriteLine("Введите свою массу");
+double mass = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите свой рост");
+double height = Convert.ToDouble(Console.ReadLine());
+double indexMass = mass / Math.Pow(height, 2);
+if (18.5 > indexMass)
+    Console.WriteLine("Вам нужно набрать вес");
+if (18.5 <= indexMass && indexMass <= 25)
+    Console.WriteLine("У вас нормальная масса тела");
+if (indexMass > 25) 
+    Console.WriteLine("Вам нужно похудеть");
+Console.WriteLine(indexMass);
+//b
+if (18.5 > indexMass)
+{
+    double massNeed = 18.5 * Math.Pow(height, 2);
+    massNeed = massNeed - mass;
+    Console.WriteLine("Вам нужно набрать {0: 0.0} кг", massNeed);
+}
+if (indexMass > 25)
+{
+    double massNeed = 25 * Math.Pow(height, 2);
+    massNeed =  mass - massNeed;
+    Console.WriteLine("Вам нужно сбросить {0: 0.0} кг", massNeed);
+}
