ff552395294b55ed8b97a901a17bd3b2ff9e9b7c
Artur <PowerBearKiller@mail.ru>
2021-12-24 22:49:35 +0300

    1stCommitLesson3

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Баранович Артур  
+//2.
+//а) С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке).
+//Требуется подсчитать сумму всех нечётных положительных чисел. Сами числа и сумму вывести на экран,
+//используя tryParse.
+int a;
+int sum = 0;
+List<int> numbers = new List<int> { };
+do
+{
+    Console.WriteLine("Введите число");
+    bool result = int.TryParse(Console.ReadLine(), out a);
+    if (result)
+    {
+        if (a > 0 && a % 2 != 0)
+        {
+            numbers.Add(a);
+            sum = sum + a;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Цифры писать нужно");
+        a = 1;
+    }
+} while (a != 0);
+foreach(int x in numbers)
+{
+    Console.WriteLine(x);
+}
+
+Console.WriteLine(" Сумма ваших нечетных, положительных чисел - " + sum);
---
+++ HomeWork3/Program.cs
@@ -0,0 +1,82 @@
+﻿//Баранович Артур
+//3. * Описать класс дробей — рациональных чисел, являющихся отношением двух целых чисел.
+//Предусмотреть методы сложения, вычитания, умножения и деления дробей. Написать программу,
+//демонстрирующую все разработанные элементы класса.
+//Добавить свойства типа int для доступа к числителю и знаменателю;
+//Добавить свойство типа double только на чтение, чтобы получить десятичную дробь числа; 
+//**Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение
+//ArgumentException("Знаменатель не может быть равен 0"); ***Добавить упрощение дробей.
+Fractions z1 = new Fractions(5,35);
+Fractions z2 = new Fractions(3, 2);
+z1.print(z1.plus(z2));
+z1.print(z1);
+int a = z2.Nod(z1);
+z1.a /= a;
+z1.b /= a;
+z1.print(z1);
+Console.WriteLine(z1.showFract);
+class Fractions
+{
+    public int a;
+    public int b;
+    public double showFract
+    {
+        get { return Convert.ToDouble(this.a) / Convert.ToDouble(this.b); }
+    }
+
+    public Fractions(int a, int b)
+    {
+        if (b == 0)
+            throw new ArgumentException($"Знаменатель не может быть равен 0");
+        this.a = a;
+        this.b = b;
+    }
+
+    public Fractions plus(Fractions x)
+    {
+        if (this.b == x.b)
+        { Fractions result = new(this.a + x.a, this.b); return result; }
+        else
+        { Fractions result = new(this.a * x.b + x.a * this.b, this.b * x.b); return result; }
+    }
+    public Fractions minus(Fractions x)
+    {
+        if (this.b == x.b)
+        { Fractions result = new(this.a - x.a, this.b); return result; }
+        else
+        { Fractions result = new(this.a * x.b - x.a * this.b, this.b * x.b); return result; }
+    }
+    public Fractions proizv(Fractions x)
+    {
+        Fractions result = new(this.a * x.a, this.b * x.b); return result; 
+    }
+    public Fractions del(Fractions x)
+    {
+        Fractions result = new(this.a * x.b, this.b * x.a); return result;
+    }
+    public void print(Fractions x)
+    {
+            Console.WriteLine($"{x.a}/{x.b}");
+    }
+    public int Nod(Fractions x)
+    {
+        int temp;
+        int c;
+        int y;
+        c = Math.Abs(x.a);
+        y = Math.Abs(x.b);
+        while (y != 0 && c != 0)
+        {
+            if (c % y > 0)
+            {
+                temp = c;
+                c = y;
+                y = temp % y;
+            }
+            else break;
+        }
+        if (y != 0 && c != 0) return y;
+        else return 0;
+    }
+
+}
